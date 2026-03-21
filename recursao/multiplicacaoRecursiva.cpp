7dd9f9a3504ccfbcd7e97eb1fc32274c66055e38
helena-varela <helena.varelakg@gmail.com>
2026-03-21 16:54:36 -0300

    multiplicação recursiva

--- recursao/multiplicacaoRecursiva.cpp
+++ recursao/multiplicacaoRecursiva.cpp
@@ -18,8 +18,23 @@
  * @return O resultado da multiplicação de X e Y.
  */
 int multiplicacao(int X, int Y) {
-    return -1;
+    if (X == 0)
+    {
+        return 0;
+    } else {
+        return  Y + multiplicacao(X - 1, Y);
+    }   
 }
+
+// ex: 3 * 2
+// X = 3, Y = 2
+// 2 + mult() -> 2 + 4 = 6
+// X = 2, Y = 2
+// 2  + mult() -> 2 + 2 = 4
+// X = 1, Y = 2
+// 2 + mult() -> 2 + 0 = 2
+// X = 0, Y = 2
+// retorna 0
 
 TEST_CASE("Multiplicar Dois Números Inteiros - Testes") {
     CHECK(multiplicacao(3, 4) == 12);
